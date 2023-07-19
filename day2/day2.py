bc06f2fada797faa98ca96b2d584ef38187aefc0
sidona <sidona.hadis18@gmail.com>
2023-07-19 08:36:04 -1200

    add solutions for day 2

---
+++ day2/day2.py
@@ -0,0 +1,27 @@
+# net=len(input("what is your name "))
+# print(net)
+
+# nett=100
+
+# print ("hi your name has "+ str(net) +"  characters")
+
+
+# twonum=(input (" please eneter 2 numbers ? "))
+# # print(type(twonum))
+
+# first=int(twonum[0])
+# second=int(twonum[1])
+
+# print(first+second)
+
+score=1
+height=1.90
+
+print(f"hi your score is {score}, your height is {height}")
+
+
+
+
+
+
+
